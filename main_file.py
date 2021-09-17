0d5d7954977e329f6862cb97831ecd2fc0ba8d50
rmichaelward <ryanwardmichael@gmail.com>
2021-09-16 19:53:22 -0700

    Create main_file.py

---
+++ main_file.py
@@ -0,0 +1,20 @@
+#main file
+#imports modules and starts GUI
+
+import PySimpleGUI as sg
+
+if __name__ == '__main__' :
+    layout = [[sg.Text("Main Menu")], [sg.Button("END")]]
+
+    # Create the window
+    window = sg.Window("Qualitative Analysis", layout)
+    
+    # Create an event loop
+    while True:
+        event, values = window.read()
+        # End program if user closes window or
+        # presses the OK button
+        if event == "END" or event == sg.WIN_CLOSED:
+            break
+
+window.close()
